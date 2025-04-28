a220ae555ec18c03ac740520a89a1649694bf02a
daniellew712 <xiaoyuew66@gmail.com>
2025-04-28 11:44:28 -0700

    wave04 added

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from ..models.planet import Planet
 from ..db import db
 
@@ -31,10 +31,49 @@ def get_all_planets():
     planets_response = []
     for planet in planets:
         planets_response.append(
-            {"id": planet.id, "name": planet.name, "description": planet.description, "size": planet.size
+            {
+                "id": planet.id, 
+                "name": planet.name, 
+                "description": planet.description, 
+                "size": planet.size
             }
         )
     return planets_response
+@planets_bp.get("/<id>")
+
+def get_one_planet(id):
+    planet = validate_planet(id)
+    
+    return {
+        "id" : planet.id,
+        "name" : planet.name,
+        "description" : planet.description,
+        "size " : planet.size 
+    }
+
+@planets_bp.put("/<id>")
+def update_planet(id):
+    planet = validate_planet(id)
+
+    request_body = request.get_json()
+
+    planet.name = request_body["name"]
+    planet.description = request_body["description"]
+    planet.size = request_body["size"]
+    db.session.commit()
+
+    return Response(status = 204, mimetype = "application/json")
+
+@planets_bp.delete("/<id>")
+def delete_planet(id):
+    planet = validate_planet(id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return Response(status = 204, mimetype = "application/json")
+
+
+
 
 
 # @planets_bp.get("/<id>")
@@ -49,14 +88,16 @@ def get_all_planets():
 #     )
 #     return planet_response
 
-# def validate_planet(id):
-#     try: 
-#         id = int(id)
-#     except: 
-#         #invalid = {"message": f"planet id {id} is invalid."}
-#         abort(make_response({"message": f"planet id {id} is invalid."}, 400))
+def validate_planet(id):
+    try: 
+        id = int(id)
+    except: 
+        #invalid = {"message": f"planet id {id} is invalid."}
+        abort(make_response({"message": f"planet id {id} is invalid."}, 400))
+
+    query = db.select(Planet).where(Planet.id == id)
+    planet = db.session.scalar(query)
+    if not planet:
+        abort(make_response({"message": f"planet id {id} is not found."}, 404))
+    return planet
     
-#     for planet in planets:
-#         if planet.id == id:
-#             return planet
-#     abort(make_response({"message": f"planet id {id} is not found."}, 404))
